594437fa5833f8fb3e69b84fcfbe8e45ac3c59b1
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2022-12-12 22:01:02 +0700

    Add Transform, Transition and Animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ module.exports = {
   darkMode: 'class',
   theme: {
     extend: {
+      animation:{
+        'spin-slow': 'spin 3s linear infinite',
+        'dance': 'wiggle 1s ease-in-out infinite'
+      },
+      keyframes:{
+        'wiggle': {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%' : { transform: 'rotate(3deg)' } 
+        }
+      },
       spacing:{
         13: '3.25rem'
       },
